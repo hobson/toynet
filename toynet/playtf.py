1837889ade25ff902e34ac0b9c3e7fdd2da840aa
Hobson Lane <github@totalgood.com>
2016-10-09 22:05:07 -0700

    false results (bad training set labels)

---
+++ toynet/playtf.py
@@ -0,0 +1,9 @@
+c = tf.constant([[1.0, 2.0], [3.0, 4.0]])
+d = tf.constant([[1.0, 1.0], [0.0, 1.0]])
+e = tf.matmul(c, d)
+
+# Construct a `Session` to execute the graph.
+sess = tf.Session()
+
+# Execute the graph and store the value that `e` represents in `result`.
+result = sess.run(e)
